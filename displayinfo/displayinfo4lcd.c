ae91e6b7576925e406f3b2bbf5b1ed3f3d67fcf8
ka1i <GuzalInspire@hotmail.com>
2019-06-18 20:20:33 +0800

    update

--- displayinfo/displayinfo4lcd.c
+++ displayinfo/displayinfo4lcd.c
@@ -16,14 +16,8 @@
 
 //Global variable
 FILE *fp;
-
 char nowtime[16];
-char nowtemp[16];
 char cpu_load[16];
-char io_used[16];
-char RAM_used[16];
-
-float cpu_temp=0;
 
 void gpio_init(void);
 void lcd1602_control(uchar cmd);
@@ -32,10 +26,9 @@ void lcd1602_setmode(void);
 void lcdprint(uchar *p,int row_number);
 
 void page1();
-void page2();
-void page3();
 
-int main(void){
+
+int main(int argc, char const *argv[]){
   wiringPiSetup();
   gpio_init();
   lcd1602_setmode();
@@ -47,9 +40,9 @@ int main(void){
     if(lcdprint_page>3){lcdprint_page=1;}
     lcd1602_control(0x01);//清屏
     switch(lcdprint_page){
-      case 1:page1();break;
-      case 2:page2();break;
-      case 3:page3();break;
+      case 1:page1(argv[1]);break;
+      //case 2:page2();break;
+      //case 3:page3();break;
       default:lcdprint_page=1;break;
     }
     lcdprint_page++;
@@ -59,29 +52,17 @@ int main(void){
 }
 
 void page1(){
-
-}
-void page2(){
-
-}
-void page3(){
-  
-}
-
-void get_now_time(){
   struct tm *ptr;
   time_t lt;
   lt=time(NULL);
   ptr=localtime(&lt);
   strftime(nowtime,16,"%I:%M:%S - %b",ptr);
-}
-
-char *get_cpu_temp(){
-  fp=popen("cat /sys/class/thermal/thermal_zone0/temp","r");
-  fscanf(fp,"%d",&cpu_temp);
-  cpu_temp=cpu_temp/1000;
-  sprintf(nowtemp,"%1f",cpu_temp);
+  lcdprint(nowtime,1);
+  fp=popen("uptime | awk '{print $9,$10,$11,$12}'","r");
+  fscanf(fp,"%s",&cpu_load);
+  lcdprint(cpu_load,2);
   pclose(fp);
+
 }
 
 void gpio_init(){
@@ -93,28 +74,12 @@ void gpio_init(){
     pinMode(28,OUTPUT); //EN Setpin
 }
 
-char *get_cpu_load(){
-  fp=popen("uptime | awk '{print $9,$10,$11,$12}'","r");
-  fscanf(fp,"%s",&load);
-  pclose(fp);
-}
-
 void lcd1602_control(uchar cmd){
   digitalWrite(27,LOW); //RS = 0
   digitalWriteByte(cmd);//GPIO0=cmd
   digitalWrite(28,HIGH);
   delay(5);
   digitalWrite(28,LOW);
-}
-
-char *get_io_used(){
-	sprintf(io_used,"%4.2f",get_io_occupy());
-}
-
-char *get_RAM_used(){
-	get_mem_occupy(&mem);
-	double using = ((double)(mem.total - mem.free)/mem.total)*100;
-	sprintf(RAM_used,"%4.2f",using);
 }
 
 void lcd1602_command(uchar dat){

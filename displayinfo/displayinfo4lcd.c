97cf3aa0e1329d2c8cbcfe19c0276d21c4ab7604
Mardan M <13086617863@126.com>
2019-10-17 14:54:27 +0800

    update

--- displayinfo/displayinfo4lcd.c
+++ displayinfo/displayinfo4lcd.c
@@ -1,119 +1,92 @@
-/*
- * Author: Mardan
- * Date: 2019.6.10
- * Project: GuzalAida
- */
+/***********************************
+本程序用于树莓派通过GPIO连接1602的效
+果演示，使用wiringPi库和其附带的LCD
+库进行GPIO和1602的操作。
 
+                 created by @qtsharp
+************************************
+示例1：
+    $sudo ./l602
+    屏幕显示：
+        ------------------
+        |  Raspberry Pi! |
+        |51.9C 215/477MB |
+        ------------------
+        
+示例2：
+    $sudo ./1602 QtSharp
+    屏幕显示：
+        ------------------
+        |QtSharp         |
+        |51.9C 215/477MB |
+        ------------------
 
-#include <time.h>
+示例3：
+    $sudo ./1602 \ \ Hello\ World
+    屏幕显示：
+        ------------------
+        |  hello world   |
+        |51.9C 215/477MB |
+        ------------------
+
+************************************/
+
 #include <stdio.h>
-#include <syslog.h>
 #include <stdlib.h>
-#include <unistd.h>
-#include <string.h>
+#include <stdint.h>
+
 #include <wiringPi.h>
+#include <lcd.h>
 
-#define uchar unsigned char
-
-//Global variable
-FILE *fp;
-char nowtime[16];
-char cpu_load[16];
-int lcdprint_page=1;
-
-void gpio_init(void);
-void lcd1602_control(uchar cmd);
-void lcd1602_command(uchar dat);
-void lcd1602_setmode(void);
-void lcdprint(uchar *p,int row_number);
-
-void page1();
-
-
-int main(int argc, char const *argv[]){
-  wiringPiSetup();
-  gpio_init();
-  lcd1602_setmode();
-  lcdprint("   Welcome to",1);
-  lcdprint(" GuzalAida CVN",2);
-  sleep(3);
-  openlog("displayinfo4lcd",LOG_CONS | LOG_PID,LOG_LOCAL0);
-  syslog(LOG_INFO,"LCD1606 display info starting...");
-  while(1){
-    lcd1602_control(0x01);//清屏
-    page1();
-    /*
-    switch(1){
-      case 1:page1(lcdprint_page);break;
-      //case 2:page2();break;
-      //case 3:page3();break;
-      default:lcdprint_page=1;break;
-    }*/
-    lcdprint_page++;
+int main (int args, char *argv[])
+{
+    if (wiringPiSetup () == -1)
+        exit (1) ;
+    int fd = lcdInit (2, 16, 8,  26,27 , 10,11,12,13,14,21,22,23) ;
+    if (fd == -1)
+    {
+        printf ("lcdInit 1 failed\n") ;
+        return 1 ;
+    }
+    sleep (1) ; //显示屏初始化
+    
+    lcdPosition (fd, 0, 0); lcdPuts (fd, "  Raspberry Pi!"); //启动信息
     sleep(1);
-  }
-  return 0;
-}
+    
+    if(argv[1])
+    {
+        lcdPosition (fd, 0, 0) ;
+        lcdPuts (fd, "                ") ; //清空第一行
+        lcdPosition (fd, 0, 0) ; lcdPuts (fd, argv[1]) ; //命令行参数显示至第一行
+    } 
 
-void page1(){
-  struct tm *ptr;
-  time_t lt;
-  lt=time(NULL);
-  ptr=localtime(&lt);
-  strftime(nowtime,16,"%I:%M:%S - %b",ptr);
-  lcdprint(nowtime,1);
-  fp=popen("uptime | awk '{print $9,$10,$11,$12}'","r");
-  fgets(cpu_load,16,fp);
-  lcdprint(cpu_load,2);
-  printf("Time: %s --- Load: %s\n",nowtime,cpu_load);
-  if(lcdprint_page>100){syslog(LOG_INFO,"Time: %s --- Load: %s\n",nowtime,cpu_load);lcdprint_page=0;}
-  pclose(fp);
+    FILE *fp;
+    char temp_char[15]; //树莓派温度
+    char Total[20]; //总内存量
+    char Free[20]; //空闲内存量
+    
+    while(1)
+    {
+        fp=fopen("/sys/class/thermal/thermal_zone0/temp","r"); //读取树莓派温度
+        fgets(temp_char,9,fp);
+        float Temp;
+        Temp=atof(temp_char)/1000;
+        lcdPosition (fd, 0, 1);lcdPrintf (fd, "%3.1fC", Temp) ; 
+        fclose(fp);
 
-}
+        fp=fopen("/proc/meminfo","r"); //读取内存信息
+        fgets(Total,19,fp);
+        fgets(Total,4,fp);
+        fgets(Free,9,fp);
+        fgets(Free,19,fp);
+        fgets(Free,4,fp);
+        lcdPosition (fd, 7, 1);
+        lcdPrintf (fd, "%3d/%3dMB", atoi(Free),atoi(Total)) ;
+        fclose(fp);
+        
+        sleep(1);
+    }
 
-void gpio_init(){
-    //data pin(0 to 7) Setpin
-    pinMode(10,OUTPUT);
-    pinMode(11,OUTPUT);
-    pinMode(12,OUTPUT);
-    pinMode(13,OUTPUT);
-    pinMode(14,OUTPUT);
-    pinMode(21,OUTPUT);
-    pinMode(22,OUTPUT);
-    pinMode(23,OUTPUT);
-
-    pinMode(26,OUTPUT); //RS Setpin
-    pinMode(27,OUTPUT); //EN Setpin
-}
-
-void lcd1602_control(uchar cmd){
-  digitalWrite(26,LOW); //RS = 0
-  digitalWriteByte(cmd);//GPIO0=cmd
-  digitalWrite(27,HIGH);
-  delay(5);
-  digitalWrite(27,LOW);
-}
-
-void lcd1602_command(uchar dat){
-  digitalWrite(26,HIGH); //RS=1
-  digitalWriteByte(dat);//GPIO0=dat
-  digitalWrite(27,HIGH);// EN=1;
-  delay(5);     
-  digitalWrite(27,LOW);// EN=0;       
-}
-
-void lcd1602_setmode(void){
-  lcd1602_control(0x38);//八线模式下两行显示 
-  lcd1602_control(0x0c);//开启显示
-  lcd1602_control(0x06);//地址+1
-  lcd1602_control(0x01);//清屏
-}
-
-void lcdprint(uchar *p,int row_number){
-  int i=0;
-  row_number==1?lcd1602_control(0x80):lcd1602_control(0xc0);
-  while(p[i]!='\0'){
-    lcd1602_command(p[i]);
-    i++;
-  }
+    return 0;
+    
 }
